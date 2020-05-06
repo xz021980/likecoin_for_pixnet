42b17b70ec92586829111cadddc1cda1afe7eb0c
xz021980 <xz021980@gmail.com>
2020-05-06 21:59:16 +0800

    20200506 更新記錄
    -修正透過 Safari 觀看時圖片顯示異常，同步修正手機版圖片顯示異常
    -採用痞客邦文章唯一識別網址傳送至 LikeCoin Server ，修正網址帶參數後拍手數量不一問題

--- script.js
+++ script.js
@@ -4,10 +4,11 @@
 			var liker_id = options['liker_id'];
             var article_id = document.getElementsByTagName('body')[0].getAttribute('data-article-id');
           	if(article_id != null && typeof liker_id != 'undefined') {
-          		var content_inner = document.querySelector('.article-content-inner'),
-              		url = window.location.href,
+          		var content_inner = $('.article-content-inner'),
+                    url_arr = window.location.href.split('-'),
+              		url = encodeURI(url_arr[0]),
             		likecoin = '<p><iframe frameborder="0" height="230" src="https://button.like.co/in/embed/'+liker_id+'/button?referrer='+url+'" width="100%"></iframe></p>';
-            		content_inner.innerHTML += likecoin;
+              	content_inner.append(likecoin);
       		}
         }
     });
