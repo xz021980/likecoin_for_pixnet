93a8161758c31a5e9c7556287f2968531851210f
xz021980 <xz021980@gmail.com>
2020-09-06 13:38:09 +0800

    修復錯誤及新增短代碼功能

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-(function ($, undefined) {
+  (function ($, undefined) {
     $.extend({
-        pixLikeCoinBox: function (options) {
-			var liker_id = options['liker_id'];
-            var article_id = document.getElementsByTagName('body')[0].getAttribute('data-article-id');
-          	if(article_id != null && typeof liker_id != 'undefined') {
-          		var content_inner = $('.article-content-inner'),
-                    url_arr = window.location.href.split('-'),
-              		url = encodeURI(url_arr[0]),
-            		likecoin = '<p><iframe frameborder="0" height="230" src="https://button.like.co/in/embed/'+liker_id+'/button?referrer='+url+'" width="100%"></iframe></p>';
-              	content_inner.append(likecoin);
-      		}
+      pixLikeCoinBox: function (options) {
+        var liker_id = options['liker_id'];
+        var likecoin_cta=options['likecoin_cta'] || [];
+        var article_id = document.getElementsByTagName('body')[0].getAttribute('data-article-id');
+        if(article_id != null && typeof liker_id != 'undefined') {
+          var content_inner = $('.article-body .article-content-inner:first-child'),
+          url_arr = window.location.href.split('-'),
+          url = encodeURIComponent(url_arr[0]),
+          likecoin = '<div class="likecoin"><iframe frameborder="0" height="190" src="https://button.like.co/in/embed/'+liker_id+'/button?referrer='+url+'" width="100%"></iframe></div>';
+
+          if(Number(likecoin_cta.length)) {
+            likecoin = '<div class="likecoin_cta">' + likecoin_cta + '</div>' + likecoin;
+          }
+
+          $(likecoin).insertBefore('.poi-section');
+
+
+          content_inner.find('p:contains([likecoin])').html(likecoin);
+
         }
+      }
     });
-})(jQuery);
+  })(jQuery);
